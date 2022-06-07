c7ef0591a0ac308872aafd80fe53dd7048141d8a
jesselynfiresta <107027698+jesselynfiresta@users.noreply.github.com>
2022-06-07 14:16:52 +0700

    Delete TakeHomeTest-main directory

--- TakeHomeTest-main/HTML/image/README.txt
+++
@@ -1,1 +0,0 @@
-all of images that I used, are from google and pinterest.
--- TakeHomeTest-main/MySQL/query2.sql
+++
@@ -1,2 +0,0 @@
-SELECT customer_data.id_cust, customer_data.name , MAX(customer_data.total) AS `Largest Total`
-FROM `customer_data` ;
